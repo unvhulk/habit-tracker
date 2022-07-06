9ef1a362eed0fc7ef1c3b07cf61b4e338afc68b1
unvhulk <unvhulk@gmail.com>
2022-07-06 14:11:37 +0530

    Removed the console.log(s)

--- src/components/Sidebar/Siderbar.jsx
+++ src/components/Sidebar/Siderbar.jsx
@@ -8,7 +8,6 @@ export const Sidebar = () => {
 	const navigate = useNavigate();
 	const location = useLocation();
 	const { logoutHandler, user } = useAuth();
-	console.log(location);
 
 	return (
 		<aside className='Home-left-container'>
@@ -16,7 +15,6 @@ export const Sidebar = () => {
 				{SidebarData.map((val, key) => {
 					//Regex to match all the sub-routes in a Route.
 					let regex = new RegExp(val.link + "/?.*", "g");
-					console.log(regex, user);
 					return (
 						<div
 							key={key}
